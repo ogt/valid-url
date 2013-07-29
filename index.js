784fa573b4c4c304f47968e800323f7d22bfa2ca
MykolaPrymak <mykola.prymak@gmail.com>
2013-07-29 10:10:37 +0300

    Update index.js
    Realize the URI/HTTP/HTTPS/Web validation

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-module.exports.is_uri = is_iri;
-module.exports.is_http_uri = is_http_iri;
-module.exports.is_https_uri = is_https_iri;
-module.exports.is_web_uri = is_web_iri;
-/* code to be written */
+url = require("url");
+util = require("util");
+
+var validURI = {
+    isURI: function (uri) {
+  	if (typeof(uri) !== 'string') {
+			return false;
+		}
+		try {
+			uri = decodeURI(uri);
+			return true;
+		} catch (e) {
+			/*
+			if (e instanceof URIError) {
+				//Eror decode uri
+				return false;
+			}
+			*/
+			// Other error
+			return false;
+		}
+	},
+	isHttpURI: function (uri) {
+			if (!this.isURI(uri)) {
+				return false;
+			}
+			var url_parts = url.parse(uri);
+			//console.log(util.inspect(url_parts, {colors: true}));
+			if (url_parts.protocol === 'http:' && !!url_parts.host) {
+				return true;
+			}
+			return false;
+	},
+	isHttpsURI: function (uri) {
+		if (!this.isURI(uri)) {
+			return false;
+		}
+		var url_parts = url.parse(uri);
+		//console.log(util.inspect(url_parts, {colors: true}));
+		if (url_parts.protocol === 'https:' && !!url_parts.host) {
+			return true;
+		}
+		return false;
+	},
+	isWebURI: function (uri) {
+		return (this.isHttpURI(uri) || this.isHttpsURI(uri));
+	}
+}
+
+// Create aliases
+validURI.is_uri = validURI.isURI;
+validURI.is_http_uri = validURI.isHttpURI;
+validURI.is_https_uri = validURI.isHttpsURI;
+validURI.is_web_uri = validURI.isWebURI;
+
+module.exports = validURI;
