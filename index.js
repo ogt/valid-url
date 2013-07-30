45ea8171186e403e571c8a39b66c1991aa4635c2
MykolaPrymak <mykola.prymak@gmail.com>
2013-07-30 09:25:21 +0300

    Update index.js
    Function isHttpURI now accept the allowHttps parameter.
    
    Rewrite code to use new functionality

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-url = require("url");
-util = require("util");
+(function(module) {
+	'use strict';
 
-var validURI = {
-    isURI: function (uri) {
-		if (typeof(uri) !== 'string') {
-			return false;
-		}
-		try {
-			var decode_uri = decodeURI(uri);
-			return uri;
-		} catch (e) {
-			/*
-			if (e instanceof URIError) {
-				//Eror decode uri
+	var
+		url = require("url"),
+		util = require("util")
+	;
+
+	var validURI = {
+		isURI: function (uri) {
+			if (typeof(uri) !== 'string') {
 				return false;
 			}
-			*/
-			// Other error
-			return false;
-		}
-	},
-	isHttpURI: function (uri) {
-			if (!this.isURI(uri)) {
-				return false;
-			}
-			var url_parts = url.parse(uri);
-			if (url_parts.protocol === 'http:' && !!url_parts.host) {
+			try {
+				var decode_uri = decodeURI(uri);
 				return uri;
+			} catch (e) {
+				/*
+				if (e instanceof URIError) {
+					//Eror decode uri
+					return false;
+				}
+				*/
+				// Other error
+				return false;
 			}
-			return false;
-	},
-	isHttpsURI: function (uri) {
-		if (!this.isURI(uri)) {
-			return false;
+		},
+		isHttpURI: function (uri, allowHttps) {
+				if (!this.isURI(uri)) {
+					return false;
+				}
+				var
+					url_parts = url.parse(uri),
+					allowedProto = ['http:']
+				;
+				if (!!allowHttps) {
+					allowedProto.push('https:');
+				}
+				if ((allowedProto.indexOf(url_parts.protocol) !== -1) && !!url_parts.host) {
+					return uri;
+				}
+				return false;
+		},
+		isHttpsURI: function (uri) {
+			return this.isHttpURI(uri, true);
+		},
+		isWebURI: function (uri) {
+			return this.isHttpURI(uri, true);
 		}
-		var url_parts = url.parse(uri);
-		if (url_parts.protocol === 'https:' && !!url_parts.host) {
-			return uri;
-		}
-		return false;
-	},
-	isWebURI: function (uri) {
-		return (this.isHttpURI(uri) || this.isHttpsURI(uri));
 	}
-}
 
-// Create aliases
-validURI.is_uri = validURI.isURI;
-validURI.is_http_uri = validURI.isHttpURI;
-validURI.is_https_uri = validURI.isHttpsURI;
-validURI.is_web_uri = validURI.isWebURI;
+	// Create aliases
+	validURI.is_uri = validURI.isURI;
+	validURI.is_http_uri = validURI.isHttpURI;
+	validURI.is_https_uri = validURI.isHttpsURI;
+	validURI.is_web_uri = validURI.isWebURI;
 
-module.exports = validURI;
+	module.exports = validURI;
+})(module);
